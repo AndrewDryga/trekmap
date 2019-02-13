8a7f372d7aa6ae972f8c243a57d7c9d5863334ac
Andrew Dryga <andrew@dryga.com>
2019-02-14 00:02:17 +0200

    Bug in port reader

--- lib/trekmap/application.ex
+++ lib/trekmap/application.ex
@@ -2,16 +2,22 @@ defmodule Trekmap.Application do
   use Application
 
   def start(_type, _args) do
-    port = System.get_env("PORT") || 4000
-
     children = [
       Trekmap.SessionManager,
       Trekmap.Bots.Guardian,
       Trekmap.Bots.GalaxyScanner,
-      Plug.Cowboy.child_spec(scheme: :http, plug: Trekmap.Router, options: [port: port])
+      Plug.Cowboy.child_spec(scheme: :http, plug: Trekmap.Router, options: [port: port()])
     ]
 
     opts = [strategy: :one_for_all, name: Trekmap.Application.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp port do
+    if port = System.get_env("PORT") do
+      String.to_integer(port)
+    else
+      4000
+    end
   end
 end
