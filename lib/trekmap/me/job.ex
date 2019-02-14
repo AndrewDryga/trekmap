ecc8f28fb5a6316c866a10440b5264d93bb4832a
Andrew Dryga <andrew@dryga.com>
2019-02-14 15:49:47 +0200

    Handle jobs with 0 duration

--- lib/trekmap/me/job.ex
+++ lib/trekmap/me/job.ex
@@ -1,10 +1,14 @@
 defmodule Trekmap.Job do
-  # TODO: add remaining duration to job
   def fetch_ship_repair_job(%{active_jobs: %{list: %{items: jobs}}, current_timestamp: cur_ts}) do
     case Enum.filter(jobs, fn item -> Map.fetch!(item, :kind) == 5 end) do
       [job | _jobs] ->
         remaining_duration = remaining_duration(job, cur_ts)
-        {:ok, %{job | remaining_duration: remaining_duration}}
+
+        if remaining_duration > 0 do
+          {:ok, %{job | remaining_duration: remaining_duration}}
+        else
+          {:error, :not_found}
+        end
 
       [] ->
         {:error, :not_found}
@@ -15,11 +19,20 @@ defmodule Trekmap.Job do
     case Enum.filter(jobs, fn item -> Map.fetch!(item, :kind) == 7 end) do
       [job | _jobs] ->
         remaining_duration = remaining_duration(job, cur_ts)
-        {:ok, %{job | remaining_duration: remaining_duration}}
+
+        if remaining_duration > 0 do
+          {:ok, %{job | remaining_duration: remaining_duration}}
+        else
+          {:error, :not_found}
+        end
 
       [] ->
         {:error, :not_found}
     end
+  end
+
+  defp remaining_duration(%{duration: 0}, _cur_ts) do
+    0
   end
 
   defp remaining_duration(
